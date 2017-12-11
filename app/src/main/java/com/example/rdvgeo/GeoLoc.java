61dc295efddf1faeba0f1f706079516414874187
sorrow33 <william.sireix@gmail.com>
2017-12-11 13:02:07 +0100

    Premier commit du projet, la sélection du contact, récuperer les coordonnées géo et envoyer un message sont implémentés.

---
+++ app/src/main/java/com/example/rdvgeo/GeoLoc.java
@@ -0,0 +1,86 @@
+package com.example.rdvgeo;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class GeoLoc extends Service {
+    private LocationManager locationMgr = null;
+
+    private LocationListener onLocationChange = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            Toast.makeText(getBaseContext(),
+                    "Voici les coordonnées de votre téléphone : " + latitude + " " + longitude,
+                    Toast.LENGTH_LONG).show();
+            sendBroadcast(true,longitude,latitude);
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        locationMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //locationMgr.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000,
+              //      0, onLocationChange);
+            locationMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0,
+                    onLocationChange);
+        }
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        locationMgr.removeUpdates(onLocationChange);
+    }
+
+    private void sendBroadcast(boolean success,double longitude,double latitude) {
+        Intent intent = new Intent ("message"); //put the same message as in the filter you used in the activity when registering the receiver
+        intent.putExtra("success", success);
+        intent.putExtra("longitude", longitude);
+        intent.putExtra("latitude", latitude);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/rdvgeo/MainActivity.java
@@ -0,0 +1,232 @@
+package com.example.rdvgeo;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.design.widget.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+    static final int SEND_SMS_REQUEST = 2;  // The request code
+    static final int READ_PHONE_STATE_REQUEST = 3;  // The request code
+    public static final String TAG = "MainActivity";
+    private String locationProvider;
+
+
+    private EditText phoneNumbers;
+    private TextView textView1;
+    private TextView textView2;
+    private TextView longitudetv;
+    private TextView latitudetv;
+
+    private double longitude;
+    private double latitude;
+
+    private BroadcastReceiver BReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //put here whaterver you want your activity to do with the intent received
+            boolean s = intent.getBooleanExtra("success", false);
+            setLongitude(intent.getDoubleExtra("longitude", 0));
+            setLatitude(intent.getDoubleExtra("latitude", 0));
+        }
+    };
+
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(BReceiver, new IntentFilter("message"));
+    }
+
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(BReceiver);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        phoneNumbers = (EditText) findViewById(R.id.contacts);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        longitudetv = (TextView) findViewById(R.id.textView4);
+        latitudetv = (TextView) findViewById(R.id.textView6);
+        startService(new Intent(MainActivity.this, GeoLoc.class));
+
+    }
+
+    public void setLongitude(Double longi) {
+        this.longitude = longi;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLatitude(Double lati) {
+        this.latitude = lati;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public void pickContact(View view) {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    public void getLoc(View view) {
+        longitudetv.setText("" + getLongitude());
+        latitudetv.setText("" + getLatitude());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check whether the result is ok
+        if (resultCode == RESULT_OK) {
+            // Check for the request code, we might be usign multiple startActivityForReslut
+            switch (requestCode) {
+                case PICK_CONTACT_REQUEST:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null;
+            String name = null;
+            // getData() method will have the Content Uri of the selected contact
+            Uri uri = data.getData();
+            //Query the content uri
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            // column index of the phone number
+            int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            // column index of the contact name
+            int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            phoneNo = formatPhoneNumber(phoneNo);
+            textView2.setText(phoneNo);
+            phoneNo += ",";
+            name = cursor.getString(nameIndex);
+            textView1.setText(name);
+            phoneNumbers.append(phoneNo);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean phoneNumberChecker(String phoneNumber) {
+        return phoneNumber.length() == 4;
+    }
+
+    public void send(View view) {
+        String message;
+        String[] phoneNumbers;
+
+        message = getMessage();
+        phoneNumbers = getPhoneNumber();
+
+        if (!message.isEmpty())
+
+            for (int i = 0; i < phoneNumbers.length; i++) {
+                if (!phoneNumberChecker(phoneNumbers[i]))
+                    Toast.makeText(this.getApplicationContext(), phoneNumbers[i] + " est invalide", Toast.LENGTH_SHORT).show();
+                else {
+                    Log.i("aloe", "azazaz");
+                    sendSMS(phoneNumbers[i], message);
+
+                }
+
+            }
+
+        else
+            Toast.makeText(this.getApplicationContext(), "Aucun message", Toast.LENGTH_SHORT).show();
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.SEND_SMS) + ContextCompat
+                .checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                    (MainActivity.this, Manifest.permission.SEND_SMS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale
+                            (MainActivity.this, Manifest.permission.READ_PHONE_STATE)) {
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Please Grant Permissions",
+                        Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission
+                                                .SEND_SMS, Manifest.permission.READ_PHONE_STATE},
+                                        SEND_SMS_REQUEST);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission
+                                .SEND_SMS, Manifest.permission.READ_PHONE_STATE},
+                        SEND_SMS_REQUEST);
+            }
+        } else {
+            //Call whatever you want
+            //myMethod();
+
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+
+    }
+    }
+
+
+    public String getMessage() {
+        return "" + getLatitude() + getLongitude();
+    }
+
+    public String formatPhoneNumber(String s) {
+        return s = s.replaceAll("\\D+", "");
+    }
+
+    public String[] getPhoneNumber() {
+        String delim = "[,]";
+        String[] tokens = (this.phoneNumbers.getText().toString()).split(delim);
+        return tokens;
+    }
+
+
+
+}
