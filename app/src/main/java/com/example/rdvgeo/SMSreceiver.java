bc5f34dc5fba93c49dfb1a6373739bd52238152d
sorrow33 <william.sireix@gmail.com>
2017-12-11 14:45:15 +0100

    Envoie et lecture du message reçu avec les coordonnées gps dans l'application

---
+++ app/src/main/java/com/example/rdvgeo/SMSreceiver.java
@@ -0,0 +1,22 @@
+package com.example.rdvgeo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SMSreceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "ON RECEIVE BROADCAST", Toast.LENGTH_LONG).show();
+        Log.d("ON ","RECEIVE");
+        SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+        SmsMessage smsMessage = msgs[0];
+        Log.d("Msg ",""+smsMessage.getDisplayMessageBody());
+    }
+}
