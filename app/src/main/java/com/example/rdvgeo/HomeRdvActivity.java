0d22691100d801e76aabc3dbe78c3508e6d851e1
sorrow33 <william.sireix@gmail.com>
2017-12-30 19:28:25 +0100

    Reception des données du message dans home

--- app/src/main/java/com/example/rdvgeo/HomeRdvActivity.java
+++ app/src/main/java/com/example/rdvgeo/HomeRdvActivity.java
@@ -1,10 +1,16 @@
 package com.example.rdvgeo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.List;
@@ -16,6 +22,7 @@ public class HomeRdvActivity extends AppCompatActivity {
     private static final String TAG = "HomeRdvActivity";
 
     RendezVousGeoLocDbHelper dbHelper;
+    BroadcastReceiver broadcastReceiver;
 
     private ListView mListView;
 
@@ -26,7 +33,6 @@ public class HomeRdvActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.rdvlist);
         dbHelper = new RendezVousGeoLocDbHelper(this);
-
         //dbHelper.onDowngrade(dbHelper,1,1);
         this.deleteDatabase(DATABASE_NAME);
         /**
@@ -46,6 +52,24 @@ public class HomeRdvActivity extends AppCompatActivity {
 
         // get all rdvs
         //dbHelper.getAllRDV();
+        BroadcastReceiver broadcastReceiver =  new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                Bundle bundle = intent.getExtras();
+
+                String incoming_number= bundle.getString("phone_num");
+                double incoming_longitude = bundle.getDouble("longitude");
+                double incoming_latitude = bundle.getDouble("latitude");
+                Log.d("incoming number", "" + incoming_number);
+                Log.d("incoming longitude", "" + incoming_longitude);
+                Log.d("incoming latitude", "" + incoming_latitude);
+                //LinearLayout layout = findViewById(R.id.linear_home);
+
+            }
+        };
+//then register receiver like that :
+        registerReceiver(broadcastReceiver, new IntentFilter("mycustombroadcast"));
     }
 
     @Override
@@ -75,6 +99,19 @@ public class HomeRdvActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (broadcastReceiver != null)
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (broadcastReceiver != null)
+        unregisterReceiver(broadcastReceiver);
+    }
 }
 
 
--- app/src/main/java/com/example/rdvgeo/SMSreceiver.java
+++ app/src/main/java/com/example/rdvgeo/SMSreceiver.java
@@ -3,15 +3,18 @@ package com.example.rdvgeo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SMSreceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
@@ -19,19 +22,36 @@ public class SMSreceiver extends BroadcastReceiver {
         Log.d("ON ", "RECEIVE");
         SmsMessage[] msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
         SmsMessage smsMessage = msgs[0];
-
-        Pattern p = Pattern.compile("([-+]?[0-9]*\\.?[0-9]+);([-+]?[0-9]*\\.?[0-9]+)");
-        Matcher m = p.matcher(smsMessage.getDisplayMessageBody());
-
+        String numero = smsMessage.getDisplayOriginatingAddress();
         Log.d("Message",smsMessage.getDisplayMessageBody());
 
-        double [] loc = receptionMessage(smsMessage.getDisplayMessageBody());
-        Log.d("Longitude ", ""+ loc[0] );
-        Log.d("Latitude ", ""+loc[1] );
+        if (isRDVGeoMessage(smsMessage.getDisplayMessageBody())) {
+            double[] loc = getLocalisation(smsMessage.getDisplayMessageBody());
+            Log.d("Longitude ", "" + loc[0]);
+            Log.d("Latitude ", "" + loc[1]);
+
+            Bundle extras = intent.getExtras();
+            Intent i = new Intent("mycustombroadcast");
+            i.putExtra("phone_num", numero);
+            i.putExtra("longitude",loc[0]);
+            i.putExtra("latitude",loc[1]);
+            context.sendBroadcast(i);
+
+        }
+        else
+            Log.d("TAG", "On ne traite pas ce sms");
 
     }
 
-    public double [] receptionMessage (String message) {
+
+    public boolean isRDVGeoMessage(String message){
+        String regexRDV = "^RDVGeo";
+        Pattern rdvPattern = Pattern.compile(regexRDV);
+        Matcher m = rdvPattern.matcher(message);
+        return m.find();
+    }
+
+    public double [] getLocalisation (String message) {
         String regexCoord = "([-+]?[0-9]*\\.?[0-9]+)";
         Pattern coordsPattern = Pattern.compile(regexCoord + ";" + regexCoord);
         Matcher m = coordsPattern.matcher(message);
